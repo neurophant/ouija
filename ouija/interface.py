ab2cd0ad7c4de4d263f74a5ef64109ec5467590b
Anton Smolin <smolin.anton@gmail.com>
2023-08-11 18:05:11 +0300

    Timeouts

--- ouija/interface.py
+++ ouija/interface.py
@@ -62,7 +62,7 @@ class Interface:
 
     async def session(self, *, index: int, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         try:
-            await asyncio.wait_for(self.session_wrapped(reader=reader, writer=writer), self.tuning.serving_timeout * 2)
+            await asyncio.wait_for(self.session_wrapped(reader=reader, writer=writer), self.tuning.serving_timeout)
         except TimeoutError:
             self.telemetry.timeout_error()
         except Exception as e:
