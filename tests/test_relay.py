8d6793b125db73648d9831b91db186dd043bd365
Anton Smolin <smolin.anton@gmail.com>
2023-08-26 15:52:16 +0300

    Unit tests: relay

--- tests/test_relay.py
+++ tests/test_relay.py
@@ -9,9 +9,9 @@ from ouija.relay import Relay
 
 @pytest.mark.asyncio
 @pytest.mark.xfail(raises=NotImplementedError)
-async def test_relay_https_handler(stream_telemetry_test, stream_tuning_test):
+async def test_relay_request_handler(telemetry_test, stream_tuning_test):
     relay = Relay(
-        telemetry=stream_telemetry_test,
+        telemetry=telemetry_test,
         tuning=stream_tuning_test,
         relay_host='127.0.0.1',
         relay_port=9000,
@@ -19,36 +19,44 @@ async def test_relay_https_handler(stream_telemetry_test, stream_tuning_test):
         proxy_port=50000,
     )
 
-    await relay.https_handler(reader=AsyncMock(), writer=AsyncMock(), remote_host='example.com', remote_port=443)
-
-
-@pytest.mark.asyncio
-async def test_stream_relay_https_handler(stream_relay_test, stream_connector_test, mocker: MockerFixture):
-    mocked_stream_connector = mocker.patch('ouija.relay.StreamConnector')
-    mocked_stream_connector.return_value = stream_connector_test
-    stream_connector_test.serve = AsyncMock()
-
-    await stream_relay_test.https_handler(
+    await relay.request_handler(
         reader=AsyncMock(),
         writer=AsyncMock(),
         remote_host='example.com',
         remote_port=443,
+        https=True,
+    )
+
+
+@pytest.mark.asyncio
+async def test_stream_relay_request_handler(stream_relay_test, stream_connector_test, mocker: MockerFixture):
+    mocked_stream_connector = mocker.patch('ouija.relay.StreamConnector')
+    mocked_stream_connector.return_value = stream_connector_test
+    stream_connector_test.serve = AsyncMock()
+
+    await stream_relay_test.request_handler(
+        reader=AsyncMock(),
+        writer=AsyncMock(),
+        remote_host='example.com',
+        remote_port=443,
+        https=True,
     )
 
     stream_connector_test.serve.assert_awaited()
 
 
 @pytest.mark.asyncio
-async def test_datagram_relay_https_handler(datagram_relay_test, datagram_connector_test, mocker: MockerFixture):
+async def test_datagram_relay_request_handler(datagram_relay_test, datagram_connector_test, mocker: MockerFixture):
     mocked_datagram_connector = mocker.patch('ouija.relay.DatagramConnector')
     mocked_datagram_connector.return_value = datagram_connector_test
     datagram_connector_test.serve = AsyncMock()
 
-    await datagram_relay_test.https_handler(
+    await datagram_relay_test.request_handler(
         reader=AsyncMock(),
         writer=AsyncMock(),
         remote_host='example.com',
         remote_port=443,
+        https=True,
     )
 
     datagram_connector_test.serve.assert_awaited()
@@ -58,35 +66,35 @@ async def test_datagram_relay_https_handler(datagram_relay_test, datagram_connec
 async def test_relay_connect_wrapped(datagram_relay_test):
     reader = AsyncMock()
     reader.readuntil = AsyncMock(return_value=b'CONNECT https://example.com:443 HTTP/1.1')
-    datagram_relay_test.https_handler = AsyncMock()
+    datagram_relay_test.request_handler = AsyncMock()
 
     await datagram_relay_test.connect_wrapped(reader=reader, writer=AsyncMock())
 
-    datagram_relay_test.https_handler.assert_awaited()
+    datagram_relay_test.request_handler.assert_awaited()
 
 
 @pytest.mark.asyncio
 async def test_relay_connect_wrapped_request_error(datagram_relay_test, mocker: MockerFixture):
     reader = AsyncMock()
     reader.readuntil = AsyncMock(return_value=b'CONNECT https://example.com:443 HTTP/1.1')
-    datagram_relay_test.https_handler = AsyncMock()
+    datagram_relay_test.request_handler = AsyncMock()
     mocked_rawparser = mocker.patch('ouija.relay.Parser')
     mocked_rawparser.return_value = Parser(data=b'')
 
     await datagram_relay_test.connect_wrapped(reader=reader, writer=AsyncMock())
 
-    datagram_relay_test.https_handler.assert_not_awaited()
+    datagram_relay_test.request_handler.assert_not_awaited()
 
 
 @pytest.mark.asyncio
-async def test_relay_connect_wrapped_method_error(datagram_relay_test):
+async def test_relay_connect_wrapped_http(datagram_relay_test):
     reader = AsyncMock()
     reader.readuntil = AsyncMock(return_value=b'GET example.com HTTP/1.1')
-    datagram_relay_test.https_handler = AsyncMock()
+    datagram_relay_test.request_handler = AsyncMock()
 
     await datagram_relay_test.connect_wrapped(reader=reader, writer=AsyncMock())
 
-    datagram_relay_test.https_handler.assert_not_awaited()
+    datagram_relay_test.request_handler.assert_awaited()
 
 
 @pytest.mark.asyncio
