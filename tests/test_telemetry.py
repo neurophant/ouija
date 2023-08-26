1fafba1a8e990ce741951fdf002d4340d8a72bc3
Anton Smolin <smolin.anton@gmail.com>
2023-08-26 15:35:11 +0300

    Unit tests: telemetry

--- tests/test_telemetry.py
+++ tests/test_telemetry.py
@@ -3,61 +3,91 @@ import datetime
 from pytest_mock import MockerFixture
 
 
-def test_stream_telemetry_link(stream_telemetry_test):
-    stream_telemetry_test.collect(active=10)
+def test_telemetry_link(telemetry_test):
+    telemetry_test.collect(active=10)
 
-    assert stream_telemetry_test.active == 10
-
-
-def test_stream_telemetry_open(stream_telemetry_test):
-    stream_telemetry_test.open()
-
-    assert stream_telemetry_test.opened == 1
+    assert telemetry_test.active == 10
 
 
-def test_stream_telemetry_close(stream_telemetry_test):
-    stream_telemetry_test.close()
+def test_telemetry_open(telemetry_test):
+    telemetry_test.open()
 
-    assert stream_telemetry_test.closed == 1
-
-
-def test_stream_telemetry_send(stream_telemetry_test, data_test):
-    stream_telemetry_test.send(data=data_test)
-
-    assert stream_telemetry_test.bytes_sent == 9
+    assert telemetry_test.opened == 1
 
 
-def test_stream_telemetry_recv(stream_telemetry_test, data_test):
-    stream_telemetry_test.recv(data=data_test)
+def test_telemetry_close(telemetry_test):
+    telemetry_test.close()
 
-    assert stream_telemetry_test.bytes_recv == 9
-
-
-def test_stream_telemetry_token_error(stream_telemetry_test):
-    stream_telemetry_test.token_error()
-
-    assert stream_telemetry_test.token_errors == 1
+    assert telemetry_test.closed == 1
 
 
-def test_stream_telemetry_timeout_error(stream_telemetry_test):
-    stream_telemetry_test.timeout_error()
+def test_telemetry_send(telemetry_test, data_test, entropy_test):
+    telemetry_test.send(data=data_test, entropy=entropy_test)
 
-    assert stream_telemetry_test.timeout_errors == 1
-
-
-def test_stream_telemetry_connection_error(stream_telemetry_test):
-    stream_telemetry_test.connection_error()
-
-    assert stream_telemetry_test.connection_errors == 1
+    assert telemetry_test.payloads_sent == 1
+    assert telemetry_test.bytes_sent == 9
+    assert telemetry_test.min_payload_size == 9
+    assert telemetry_test.max_payload_size == 9
 
 
-def test_stream_telemetry_serving_error(stream_telemetry_test):
-    stream_telemetry_test.serving_error()
+def test_telemetry_recv(telemetry_test, data_test, entropy_test):
+    telemetry_test.recv(data=data_test, entropy=entropy_test)
 
-    assert stream_telemetry_test.serving_errors == 1
+    assert telemetry_test.payloads_recv == 1
+    assert telemetry_test.bytes_recv == 9
+    assert telemetry_test.min_payload_size == 9
+    assert telemetry_test.max_payload_size == 9
 
 
-def test_stream_telemetry(stream_telemetry_test, mocker: MockerFixture):
+def test_telemetry_processing_error(telemetry_test):
+    telemetry_test.processing_error()
+
+    assert telemetry_test.processing_errors == 1
+
+
+def test_telemetry_token_error(telemetry_test):
+    telemetry_test.token_error()
+
+    assert telemetry_test.token_errors == 1
+
+
+def test_telemetry_timeout_error(telemetry_test):
+    telemetry_test.timeout_error()
+
+    assert telemetry_test.timeout_errors == 1
+
+
+def test_telemetry_connection_error(telemetry_test):
+    telemetry_test.connection_error()
+
+    assert telemetry_test.connection_errors == 1
+
+
+def test_telemetry_serving_error(telemetry_test):
+    telemetry_test.serving_error()
+
+    assert telemetry_test.serving_errors == 1
+
+
+def test_telemetry_resending_error(telemetry_test):
+    telemetry_test.resending_error()
+
+    assert telemetry_test.resending_errors == 1
+
+
+def test_telemetry_send_buf_overload(telemetry_test):
+    telemetry_test.send_buf_overload()
+
+    assert telemetry_test.send_buf_overloads == 1
+
+
+def test_telemetry_recv_buf_overload(telemetry_test):
+    telemetry_test.recv_buf_overload()
+
+    assert telemetry_test.recv_buf_overloads == 1
+
+
+def test_telemetry(telemetry_test, mocker: MockerFixture):
     timestamp = datetime.datetime.now()
     mocked_datetime = mocker.patch('ouija.telemetry.datetime')
     mocked_datetime.datetime.now.return_value = timestamp
@@ -65,117 +95,15 @@ def test_stream_telemetry(stream_telemetry_test, mocker: MockerFixture):
         f'{timestamp}\n' \
         f'\tactive: 0\n' \
         f'\topened|closed: 0|0\n' \
+        f'\tpayloads sent|received: 0|0\n' \
         f'\tbytes sent|received: 0|0\n' \
-        f'\ttoken|timeout|connection|serving errors: 0|0|0|0'
-
-    assert str(stream_telemetry_test) == expected
-
-
-def test_datagram_telemetry_link(datagram_telemetry_test):
-    datagram_telemetry_test.collect(active=10)
-
-    assert datagram_telemetry_test.active == 10
-
-
-def test_datagram_telemetry_open(datagram_telemetry_test):
-    datagram_telemetry_test.open()
-
-    assert datagram_telemetry_test.opened == 1
-
-
-def test_datagram_telemetry_close(datagram_telemetry_test):
-    datagram_telemetry_test.close()
-
-    assert datagram_telemetry_test.closed == 1
-
-
-def test_datagram_telemetry_send(datagram_telemetry_test, data_test):
-    datagram_telemetry_test.send(data=data_test)
-
-    assert datagram_telemetry_test.packets_sent == 1
-    assert datagram_telemetry_test.bytes_sent == 9
-    assert datagram_telemetry_test.min_packet_size == 9
-    assert datagram_telemetry_test.max_packet_size == 9
-
-
-def test_datagram_telemetry_recv(datagram_telemetry_test, data_test):
-    datagram_telemetry_test.recv(data=data_test)
-
-    assert datagram_telemetry_test.packets_recv == 1
-    assert datagram_telemetry_test.bytes_recv == 9
-    assert datagram_telemetry_test.min_packet_size == 9
-    assert datagram_telemetry_test.max_packet_size == 9
-
-
-def test_datagram_telemetry_processing_error(datagram_telemetry_test):
-    datagram_telemetry_test.processing_error()
-
-    assert datagram_telemetry_test.processing_errors == 1
-
-
-def test_datagram_telemetry_token_error(datagram_telemetry_test):
-    datagram_telemetry_test.token_error()
-
-    assert datagram_telemetry_test.token_errors == 1
-
-
-def test_datagram_telemetry_type_error(datagram_telemetry_test):
-    datagram_telemetry_test.type_error()
-
-    assert datagram_telemetry_test.type_errors == 1
-
-
-def test_datagram_telemetry_timeout_error(datagram_telemetry_test):
-    datagram_telemetry_test.timeout_error()
-
-    assert datagram_telemetry_test.timeout_errors == 1
-
-
-def test_datagram_telemetry_connection_error(datagram_telemetry_test):
-    datagram_telemetry_test.connection_error()
-
-    assert datagram_telemetry_test.connection_errors == 1
-
-
-def test_datagram_telemetry_serving_error(datagram_telemetry_test):
-    datagram_telemetry_test.serving_error()
-
-    assert datagram_telemetry_test.serving_errors == 1
-
-
-def test_datagram_telemetry_resending_error(datagram_telemetry_test):
-    datagram_telemetry_test.resending_error()
-
-    assert datagram_telemetry_test.resending_errors == 1
-
-
-def test_datagram_telemetry_send_buf_overload(datagram_telemetry_test):
-    datagram_telemetry_test.send_buf_overload()
-
-    assert datagram_telemetry_test.send_buf_overloads == 1
-
-
-def test_datagram_telemetry_recv_buf_overload(datagram_telemetry_test):
-    datagram_telemetry_test.recv_buf_overload()
-
-    assert datagram_telemetry_test.recv_buf_overloads == 1
-
-
-def test_datagram_telemetry(datagram_telemetry_test, mocker: MockerFixture):
-    timestamp = datetime.datetime.now()
-    mocked_datetime = mocker.patch('ouija.telemetry.datetime')
-    mocked_datetime.datetime.now.return_value = timestamp
-    expected = \
-        f'{timestamp}\n' \
-        f'\tactive: 0\n' \
-        f'\topened|closed: 0|0\n' \
-        f'\tpackets sent|received: 0|0\n' \
-        f'\tbytes sent|received: 0|0\n' \
-        f'\tmin|max packet size: 0|0\n' \
-        f'\tprocessing|token|type errors: 0|0' \
-        f'|0\n' \
-        f'\ttimeout|connection|serving|resending errors: 0|0' \
-        f'|0|0\n' \
+        f'\tmin|avg|max payload size: ' \
+        f'0|0|0\n' \
+        f'\tmin|avg|max entropy: 0.0000|0.0000|0.0000\n' \
+        f'\ttoken errors: 0\n' \
+        f'\tprocessing|resending errors: 0|0\n' \
+        f'\ttimeout|connection|serving errors: ' \
+        f'0|0|0\n' \
         f'\tsend|recv buf overloads: 0|0'
 
-    assert str(datagram_telemetry_test) == expected
+    assert str(telemetry_test) == expected
